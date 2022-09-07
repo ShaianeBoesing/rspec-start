a09397a71d33d05af7e3a5acc6005ec6186d51c9
Shaiane Boesing <shaianeboesingrb@gmail.com>
2022-09-07 15:46:04 -0300

    :white_check_mark: test: aplicando a filosofia DRY nos testes através do factory bot

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do 
     factory :user do
-        nickname{ FFaker::Lorem.word }
+        nickname{ (FFaker::Lorem.word).capitalize }
         level{ FFaker::Random.rand(1..99) }
         kind{ %i[ knight wizard].sample }
     end
